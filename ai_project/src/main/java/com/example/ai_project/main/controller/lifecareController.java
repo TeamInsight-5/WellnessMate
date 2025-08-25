77fbfe9ba54cc5a43da017ceee0d485a3b838ae8
yb2006 <yunbin1117@gmail.com>
2025-08-25 11:31:18 +0900

    기본 구조(수정)

---
+++ ai_project/src/main/java/com/example/ai_project/main/controller/lifecareController.java
@@ -0,0 +1,28 @@
+package com.example.ai_project.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class lifecareController {
+
+
+    // 메인화면
+    @GetMapping("/main" )
+    public String mainpage(){
+        return "main/mainpage";
+    }
+
+    // 심리케어
+    @GetMapping("/trial")
+    public String trialcare(){
+        return "trial/PsychologicalCare";
+    }
+
+    // 내과
+    @GetMapping("/medicine")
+    public String medicine(){
+        return "medicine/medicine";
+    }
+
+}
