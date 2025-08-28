73ac12729bb8cc0fcce280d8ae55181c455aa1b1
yb2006 <yunbin1117@gmail.com>
2025-08-28 14:41:08 +0900

    security 수정

--- ai_project/src/main/java/com/example/ai_project/main/controller/WellnessMateController.java
+++ ai_project/src/main/java/com/example/ai_project/main/controller/WellnessMateController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LifecareController {
+public class WellnessMateController {
 
 
     // 메인화면
